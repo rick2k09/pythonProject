0d98d7a7a91e2de1cecb3dcf1fb912788bcb9302
Hyde <>
2021-07-21 17:11:50 +0800

    modify to fix mac environment

--- test_ImageCompare.py
+++ test_ImageCompare.py
@@ -1,3 +1,7 @@
+import os,sys
+dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(dir)
+sys.path.append("/Users/hyde/pythonProject/venv/lib/site-packages")
 import unittest
 from wand.image import Image
 import time
@@ -5,9 +9,9 @@ import os
 
 class CompareImages(unittest.TestCase):
     def test_compares(self):
-        folder1 = 'C:/Users/PERFECT/Pictures/80/'
-        folder2 = 'C:/Users/PERFECT/Pictures/100/'
-        resultfolder = 'C:/Users/PERFECT/Pictures/test4results/'
+        folder1 = '/Users/hyde/unittest_server/YMK_unit_test/base/'
+        folder2 = '/Users/hyde/unittest_server/YMK_unit_test/upload/'
+        resultfolder = '/Users/hyde/unittest_server/YMK_unit_test/test4results/'
         d1 = os.listdir(folder1)
         d2 = os.listdir(folder2)
         cropsize = 0.05
@@ -15,23 +19,24 @@ class CompareImages(unittest.TestCase):
         files = list(set(d1) & set(d2))
 
         for i in files:
-            with Image(filename=folder1 + i) as base:
-                with Image(filename=folder2 + i) as img:
-                    #Crop left, top, right, bottom
-                    base.crop(int(base.width*cropsize), int(base.height*cropsize), int(base.width*(1-cropsize)), int(base.height*(1-cropsize)))
-                    img.crop(int(img.width*cropsize), int(img.height*cropsize), int(img.width*(1-cropsize)), int(img.height*(1-cropsize)))
-                    #Compare
-                    base.fuzz = base.quantum_range * 0.01  # Threshold of 1%
-                    result_image, result_metric = base.compare(img, 'absolute')
-                    #Save Difference
-                    result_image.save(filename=resultfolder + i)
-                    with self.subTest(msg=resultfolder+i, i=i, time=(time.time()-current_time)):
-                        if result_metric == 0:
-                            self.assertEqual(result_metric, 0)
-                        else:
-                            self.assertEqual(result_metric, 0, f'photo ({i}) is Fail')
+            if not i.startswith('.') and os.path.isfile(os.path.join(folder1, i)):
+                with Image(filename=folder1 + i) as base:
+                    with Image(filename=folder2 + i) as img:
+                        # Crop left, top, right, bottom
+                        base.crop(int(base.width*cropsize), int(base.height*cropsize), int(base.width*(1-cropsize)), int(base.height*(1-cropsize)))
+                        img.crop(int(img.width*cropsize), int(img.height*cropsize), int(img.width*(1-cropsize)), int(img.height*(1-cropsize)))
+                        # Compare
+                        base.fuzz = base.quantum_range * 0.01  # Threshold of 1%
+                        result_image, result_metric = base.compare(img, 'absolute')
+                        # Save Difference
+                        result_image.save(filename=resultfolder + i)
+                        with self.subTest(msg=resultfolder+i, i=i, time=(time.time()-current_time)):
+                            if result_metric == 0:
+                                self.assertEqual(result_metric, 0)
+                            else:
+                                self.assertEqual(result_metric, 0, f'photo ({i}) is Fail')
 
-        #print("Compare result ok, execution time = %s (s)" % (time.time() - current_time))
+        # print("Compare result ok, execution time = %s (s)" % (time.time() - current_time))
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test_suite.py
@@ -0,0 +1,23 @@
+import unittest
+import time
+from htmltestrunner import HTMLTestRunner
+from YMK_unit_test import MyTestCase
+
+if __name__ == '__main__':
+    # unittest.main()
+    # suite = unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+    # unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestSuite()
+    test1 = unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
+    test2 = unittest.defaultTestLoader.discover('./', pattern='test_ImageCompare.py')
+    suite.addTests([test1, test2])
+    # suite.addTests(test2)
+
+    timestr = time.strftime('%Y%m%d', time.localtime(time.time()))  # 本地日期作為報告名字
+    filename = '/Users/hyde/unittest_server/YMK_unit_test/testreport/'  # 文件名字及保存路徑
+    fp = open(filename + (timestr + '.html'), 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Test Result', description='Test Report： ')
+    # runner = HTMLReport.TestRunner(title='Test Result', description='Test Report： ')
+    runner.run(suite)
+    fp.close()
+    # test_SendReport.send(filename, timestr)
