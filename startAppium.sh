f53348c0348252ed7046aa075680ac764913bb0e
Hyde <>
2021-07-26 21:05:53 +0800

    no message

--- startAppium.sh
+++ startAppium.sh
@@ -1,1 +1,2 @@
-nohup appium --address 127.0.0.1 --port 4723 --session-override --log-timestamp --local-timezone  >> appium.log 2>&1&
+pm2 start appium -- -a 127.0.0.1
+# nohup appium --address 127.0.0.1 --port 4723 --session-override --log-timestamp --local-timezone  >> appium.log 2>&1&
